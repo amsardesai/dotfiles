57dc7ce38686e521671d21035ec38cf64d3d7254
Ankit Sardesai <amsardesai@gmail.com>
2015-03-20 05:09:06 -0400

    Added stuff to shell script

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 
 set -e
 
-# Set up Brew if we're in linux
-# if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  # tput setaf 1; echo "Setting up Homebrew for Linux..."; tput sgr0
-  # ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
-  # ~/.linuxbrew/bin/brew update
-  # ~/.linuxbrew/bin/brew upgrade
-  # ~/.linuxbrew/bin/brew install cmake vim git
-# fi
-
 # Set up Vim
-tput setaf 1; echo "Setting up Vim..."; tput sgr0
+echo "Setting up Vim..."
+echo ""
 
-tput setaf 1; echo " -> Vundle"; tput sgr0
+echo " -> Vundle"
+echo ""
+
 mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/swaps
+mkdir -p ~/.vim/backups
+mkdir -p ~/.vim/undo
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c PluginInstall -c quitall
 
-tput setaf 1; echo " -> YouCompleteMe"; tput sgr0
+echo " -> YouCompleteMe"
+echo ""
+
 ~/.vim/bundle/YouCompleteMe/install.sh
 
