8ff316c7ee90c28613e3d1d8d7d10cfc14ae416a
Ankit Sardesai <amsardesai@gmail.com>
2015-03-23 19:31:02 -0400

    added inputrc to setup script

--- setup.sh
+++ setup.sh
@@ -28,6 +28,7 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 ln -s ~/.vimrc "$SCRIPTPATH/.vimrc"
+ln -s ~/.inputrc "$SCRIPTPATH/.inputrc"
 ln -s ~/.ycm_extra_conf.py "$SCRIPTPATH/.ycm_extra_conf.py"
 
 
