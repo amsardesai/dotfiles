06944ec80b62a964cc1d423688dc8201dd92d880
Ankit Sardesai <ankit.sardesai@adroll.com>
2016-01-26 15:27:53 -0800

    pretty colors

--- setup.sh
+++ setup.sh
@@ -1,52 +1,58 @@
 #!/bin/bash
 
+echo_message() {
+	tput setaf 6 && printf "\n => "
+	tput setaf 3 && printf "$@\n"
+	tput sgr0
+}
+
+echo_task() {
+	tput setaf 6 && printf "\n => "
+	tput setaf 2 && printf "$@\n"
+	tput sgr0
+}
+
 set -e
 
 SCRIPTPATH=$(pwd -P)
 
-echo "Directory with scripts is $SCRIPTPATH"
+echo_message "Directory with scripts is $SCRIPTPATH"
 
-echo "Setting up bash_profile..."
+echo_task "Setting up bash_profile..."
 
 if ! [ -f ~/.bash_profile ] || ! ( grep -Fxq "source $SCRIPTPATH/.profile" ~/.bash_profile ); then
 	echo "\nsource $SCRIPTPATH/.profile\n" >> ~/.bash_profile
 	source ~/.bash_profile
 fi
 
-echo "Setting up files..."
+echo_task "Setting up files..."
 
-if ! [ -f "git-prompt.bash" ]; then
-	curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh \
-		> git-prompt.bash
-fi
+curl -# https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh \
+	> git-prompt.bash
 
-if ! [ -f "git-completion.bash" ]; then
-	curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
-		> git-completion.bash
-fi
+curl -# https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
+	> git-completion.bash
 
-if ! [ -f ".ycm_extra_conf.py" ]; then
-	curl https://raw.githubusercontent.com/Valloric/ycmd/master/cpp/ycm/.ycm_extra_conf.py \
-		> .ycm_extra_conf.py
-fi
+curl -# https://raw.githubusercontent.com/Valloric/ycmd/master/cpp/ycm/.ycm_extra_conf.py \
+	> .ycm_extra_conf.py
 
-echo "Creating aliases..."
+echo_task "Creating aliases..."
 
-ln -sv ~/.vimrc "$SCRIPTPATH/.vimrc"
-ln -sv ~/.gvimrc "$SCRIPTPATH/.gvimrc"
-ln -sv ~/.inputrc "$SCRIPTPATH/.inputrc"
-ln -sv ~/.ycm_extra_conf.py "$SCRIPTPATH/.ycm_extra_conf.py"
-ln -sv ~/.tern-config "$SCRIPTPATH/.tern-config"
+ln -svf "$SCRIPTPATH/.vimrc" "$HOME/.vimrc"
+ln -svf "$SCRIPTPATH/.gvimrc" "$HOME/.gvimrc"
+ln -svf "$SCRIPTPATH/.inputrc" "$HOME/.inputrc"
+ln -svf "$SCRIPTPATH/.ycm_extra_conf.py" "$HOME/.ycm_extra_conf.py"
+ln -svf "$SCRIPTPATH/.tern-config" "$HOME/.tern-config"
 
-echo "Setting up vim..."
+echo_task "Setting up vim..."
 
 mkdir -p ~/.vim
 
-echo "Setting up neovim aliases..."
+echo_task "Setting up neovim aliases..."
 
-mkdir -p $XDG_CONFIG_HOME/nvim
-ln -sv ~/.vim $XDG_CONFIG_HOME/nvim
-ln -sv ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+mkdir -p $XDG_CONFIG_HOME
+ln -svf "$HOME/.vim" "$XDG_CONFIG_HOME/nvim"
+ln -svf "$HOME/.vimrc" "$XDG_CONFIG_HOME/nvim/init.vim"
 
-echo "To finish, open vim and run :PlugInstall"
+echo_message "To finish, open vim and run :PlugInstall"
 
