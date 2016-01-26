043d75bb5e3fb39ca9fcb3da8d3fa26d68814166
Ankit Sardesai <ankit.sardesai@adroll.com>
2016-01-26 14:58:34 -0800

    script

--- setup.sh
+++ setup.sh
@@ -2,29 +2,41 @@
 
 set -e
 
+SCRIPTPATH=$(pwd -P)
+
+echo "Directory with scripts is $SCRIPTPATH"
+
 echo "Setting up bash_profile..."
 
-echo "\nsource $SCRIPTPATH/.profile" >> ~/.bash_profile
-source ~/.bash_profile
+if ! [ -f ~/.bash_profile ] || ! ( grep -Fxq "source $SCRIPTPATH/.profile" ~/.bash_profile ); then
+	echo "\nsource $SCRIPTPATH/.profile\n" >> ~/.bash_profile
+	source ~/.bash_profile
+fi
 
 echo "Setting up files..."
 
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh \
-	> git-prompt.bash
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
-	> git-completion.bash
-curl https://raw.githubusercontent.com/Valloric/ycmd/master/cpp/ycm/.ycm_extra_conf.py \
-	> .ycm_extra_conf.py
+if ! [ -f "git-prompt.bash" ]; then
+	curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh \
+		> git-prompt.bash
+fi
+
+if ! [ -f "git-completion.bash" ]; then
+	curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
+		> git-completion.bash
+fi
+
+if ! [ -f ".ycm_extra_conf.py" ]; then
+	curl https://raw.githubusercontent.com/Valloric/ycmd/master/cpp/ycm/.ycm_extra_conf.py \
+		> .ycm_extra_conf.py
+fi
 
 echo "Creating aliases..."
 
-SCRIPTPATH=$(pwd -P)
-
-ln -s ~/.vimrc "$SCRIPTPATH/.vimrc"
-ln -s ~/.gvimrc "$SCRIPTPATH/.gvimrc"
-ln -s ~/.inputrc "$SCRIPTPATH/.inputrc"
-ln -s ~/.ycm_extra_conf.py "$SCRIPTPATH/.ycm_extra_conf.py"
-ln -s ~/.tern-config "$SCRIPTPATH/.tern-config"
+ln -sv ~/.vimrc "$SCRIPTPATH/.vimrc"
+ln -sv ~/.gvimrc "$SCRIPTPATH/.gvimrc"
+ln -sv ~/.inputrc "$SCRIPTPATH/.inputrc"
+ln -sv ~/.ycm_extra_conf.py "$SCRIPTPATH/.ycm_extra_conf.py"
+ln -sv ~/.tern-config "$SCRIPTPATH/.tern-config"
 
 echo "Setting up vim..."
 
@@ -32,9 +44,9 @@ mkdir -p ~/.vim
 
 echo "Setting up neovim aliases..."
 
-mkdir -p $XDG_CONFIG_HOME
-ln -s ~/.vim $XDG_CONFIG_HOME/nvim
-ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+mkdir -p $XDG_CONFIG_HOME/nvim
+ln -sv ~/.vim $XDG_CONFIG_HOME/nvim
+ln -sv ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 
 echo "To finish, open vim and run :PlugInstall"
 
